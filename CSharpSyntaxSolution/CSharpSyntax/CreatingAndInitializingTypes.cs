579c2251894570925ce356ca827349efce5ad8b6
Brittny Williams <brittnyaw@gmail.com>
2023-02-07 15:41:29 -0500

    2/7

---
+++ CSharpSyntaxSolution/CSharpSyntax/CreatingAndInitializingTypes.cs
@@ -0,0 +1,26 @@
+﻿namespace CSharpSyntax
+{
+    public class CreatingAndInitializingTypes
+    {
+        [Fact]
+
+        public void UsingLiteralsToCreateInstancesOfTypes()
+        {
+
+
+            string myName = "jeff";
+            char me = 'a';
+
+            bool isVendor = true;
+            int myAge = 22;
+
+            bool isLegalAgeToDrink = myAge >= 21;
+
+            Taco food = new Taco();
+            Assert.Equal("jeff", myName);
+            Assert.Equal(22, myAge);
+        }
+    }
+
+    public class Taco { }
+}
---
+++ Practices/DjBreakTimerSolution/DjBreakTimer/Program.cs
@@ -0,0 +1,24 @@
+﻿// iteration 1
+Console.Write("How long is the break");
+var breakDouble = double.Parse(Console.ReadLine());
+
+Console.Write("How long is the song");
+var songLength = TimeSpan.Parse(Console.ReadLine());
+var totalMinutes = songLength.TotalMinutes;
+
+
+var endBreak = DateTimeOffset.Now.AddMinutes(breakDouble);
+
+Console.Write($"At the end of the break it will be {endBreak}\n\n");
+
+Console.Write($"The user should start the song at {endBreak.AddSeconds(totalMinutes * -1)}");
+
+//Iteration 2
+
+var timeRemaining = breakDouble;
+while (timeRemaining > 0)
+{
+    Console.Write($"There are {timeRemaining} minutes left in the break\n");
+    Thread.Sleep(1000);
+    timeRemaining -= 1;
+}
