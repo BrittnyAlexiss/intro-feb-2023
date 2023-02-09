0699dc3ddd20e8469937ec9cd90cfd120002bc55
Brittny Williams <brittnyaw@gmail.com>
2023-02-09 11:44:35 -0500

    Inheritance

---
+++ Practices/stringcalculator-master/StringCalculatorTests.cs
@@ -0,0 +1,41 @@
+﻿namespace StringCalculator; public class StringCalculatorTests
+{
+    [Fact]
+    public void EmptyStringReturnsZero()
+    {
+        var calculator = new StringCalculator(); var result = calculator.Add(""); Assert.Equal(0, result);
+    }
+    [Theory]
+    [InlineData("1", 1)]
+    [InlineData("2", 2)]
+    public void StringWithOneNumberReturnsOne(string numbers, int expected)
+    {
+        var calculator = new StringCalculator(); var result = calculator.Add(numbers); Assert.Equal(expected, result);
+    }
+    [Theory]
+    [InlineData("1,2", 3)]
+    [InlineData("2,3", 5)]
+    public void StringWithTwoNumberReturnsSum(string numbers, int expected)
+    {
+        var calculator = new StringCalculator(); var result = calculator.Add(numbers); Assert.Equal(expected, result);
+    }
+    [Theory]
+    [InlineData("1,2,3,4,5,6,7,8,9", 45)]
+    public void StringWithUnkownAmountNumberReturnsSum(string numbers, int expected)
+    {
+        var calculator = new StringCalculator(); var result = calculator.Add(numbers); Assert.Equal(expected, result);
+    }
+    [Theory]
+    [InlineData("1,2,3,4,5,6,7,8,9\n1,2", 48)]
+    public void StringWithNewLineDelimiterReturnsSum(string numbers, int expected)
+    {
+        var calculator = new StringCalculator(); var result = calculator.Add(numbers); Assert.Equal(expected, result);
+    }
+    [Theory]
+    [InlineData("//;\n1;2", 3)]
+    [InlineData("//*;\n8*2,1\n1", 12)]
+    public void StringWithDifferentDelimiterReturnsSum(string numbers, int expected)
+    {
+        var calculator = new StringCalculator(); var result = calculator.Add(numbers); Assert.Equal(expected, result);
+    }
+}
